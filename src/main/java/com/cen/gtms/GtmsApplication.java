9d721589095409e2125e55c4d0a4588fa56a5393
camonologue <2339695474@qq.com>
2022-09-14 10:26:17 +0800

    first commit

---
+++ src/main/java/com/cen/gtms/GtmsApplication.java
@@ -0,0 +1,13 @@
+package com.cen.gtms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cen/gtms/GtmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cen.gtms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
